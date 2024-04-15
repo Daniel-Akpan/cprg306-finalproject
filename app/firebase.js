c533d3e6fd49637da9eb510310a47c029b80c4db
Daniel-Akpan <95925992+TW-Warlord@users.noreply.github.com>
2024-04-15 13:33:49 -0600

    removing analytics

--- app/firebase.js
+++ app/firebase.js
@@ -1,9 +1,16 @@
 // Import necessary Firebase modules
 import { initializeApp } from "firebase/app";
-import { isSupported as isAnalyticsSupported, getAnalytics } from "firebase/analytics";
+// import { isSupported as isAnalyticsSupported, getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage"; 
-import { getFirestore, doc, setDoc, updateDoc, onSnapshot, getDoc } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  updateDoc,
+  onSnapshot,
+  getDoc,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,29 +20,40 @@ const firebaseConfig = {
   storageBucket: "cprg306-todoapp.appspot.com",
   messagingSenderId: "895948289557",
   appId: "1:895948289557:web:f7b46a1d2d7c047e90001c",
-  measurementId: "G-DHVKE9BEX3"
+  measurementId: "G-DHVKE9BEX3",
 };
 
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 const firebaseStorage = getStorage(app);
 
 // Function to update user document in Firestore
 async function updateUser(uid, userData) {
-  const userDocRef = doc(firestore, 'users', uid);
+  const userDocRef = doc(firestore, "users", uid);
   try {
     await setDoc(userDocRef, userData, { merge: true });
-    console.log('User data updated successfully');
+    console.log("User data updated successfully");
   } catch (error) {
-    console.error('Error updating user data:', error);
+    console.error("Error updating user data:", error);
   }
 }
 
 // Export the auth object, updateUser function, doc, and onSnapshot
-export { auth, firestore, updateUser, doc, updateDoc, setDoc, getDoc, onSnapshot, firebaseStorage, analytics };
+export {
+  auth,
+  firestore,
+  updateUser,
+  doc,
+  updateDoc,
+  setDoc,
+  getDoc,
+  onSnapshot,
+  firebaseStorage,
+  analytics,
+};
 
 // Verify Firestore import
 if (!firestore) {
