90e69cd44d989051693bbcd09c5cf8a4811c788e
CooperA02 <cooper.armstrong@icloud.com>
2024-04-16 18:25:58 -0600

    Home page styling complete

--- app/page.js
+++ app/page.js
@@ -11,20 +11,29 @@ const Page = () => {
         backgroundPosition: "center",
       }}
     >
-      <h1 className="text-4xl font-bold mb-8 text-center">
-        Welcome to our to-do app
+      <h1 className="text-4xl font-bold mb-4 text-center">
+        Organize your work <br></br> and life, finally.
       </h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <Link href="/sign-up">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded block text-center">
-            Sign Up
-          </button>
-        </Link>
-        <Link href="/log-in">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded block text-center">
-            Login
-          </button>
-        </Link>
+      <p className="text-lg mb-8 text-center">
+        Become focused, organized, <br></br>and calm with our To-do-list
+      </p>
+      <div className="p-4 border border-black rounded-md max-w-md mx-auto bg-white">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div>
+            <Link href="/sign-up">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded block text-center">
+                Sign Up
+              </button>
+            </Link>
+          </div>
+          <div>
+            <Link href="/log-in">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded block text-center">
+                Login
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
